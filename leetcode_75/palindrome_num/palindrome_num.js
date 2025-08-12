9fab9841776aac12073ae5398571fb1347b0eb79
SorrenJ <soalexjao@gmail.com>
2025-08-12 15:59:38 -0700

    add palindrome number checker solution

---
+++ leetcode_75/palindrome_num/palindrome_num.js
@@ -0,0 +1,65 @@
+/*
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+
+1. break the it into chars
+
+
+
+*/
+
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    
+    //convert the number into a string and break the number into separate characters
+
+
+let stringX = x.toString();
+console.log('string:', stringX);
+
+let chars= stringX.split('');
+
+let reverse = chars.reverse().join('');
+
+console.log('reverse:', reverse);
+if(stringX === reverse){
+   
+    return true
+} 
+
+
+return false;
+};
+
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
